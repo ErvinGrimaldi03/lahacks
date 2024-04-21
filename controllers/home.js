edb6b56cc6c51cb44b24fc504862476ac7f57307
leo <hermosilloleoncio@gmail.com>
2024-04-21 03:59:30 -0700

    Save before pull

--- controllers/home.js
+++ controllers/home.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const middleware = require('../config/middleware');
 
+
 /*
-
 // Create WebSocket connection.
-//const socket = new WebSocket("wss://api.hume.ai/v0/stream/models");
-
-
+const socket = new WebSocket("wss://api.hume.ai/v0/stream/models");
 
 // Connection opened
-//socket.addEventListener("open", (event) => {
-//   socket.send("Hello Server!");
-// });
+socket.addEventListener("open", (event) => {
+   socket.send("Hello Server!");
+});
 
 // Listen for messages
-//socket.addEventListener("message", (event) => {
-    // console.log("Message from server ", event.data);
-//});
+socket.addEventListener("message", (event) => {
+     console.log("Message from server ", event.data);
+});
 */
+
 
 module.exports.index = async (req, res) => {
     res.render('index');
